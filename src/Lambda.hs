65195c2e8db682d750cfd3f2cc656c4ad2bdc731
Yury Sukhoverkhov <y>
2017-05-26 23:12:23 +0300

    Implementing syntax

--- src/Lambda.hs
+++ src/Lambda.hs
@@ -2,8 +2,10 @@ module Lambda where
 
 -- TODO: specify terms to export.
 
-import Data.List (find)
+import Data.List (find, elemIndex)
 import Data.Maybe (fromMaybe)
+
+-- Data types
 
 data Term =
   TmVar Int Int |
@@ -11,27 +13,70 @@ data Term =
   TmApp Term Term
   deriving (Show)
 
-newtype Context = Context [(String, Binding)]
+data NameBind = NameBind
+type Binding = NameBind
 
-type Binding = Int
+type Name = String
 
-printTm :: Context -> Term -> String
-printTm ctx t = case t of
-  TmVar x n | ctxLength ctx == n -> show (index2name ctx x)
-            | otherwise -> "[bad index]"
-  TmAbs t1 x ->
-    let (ctx', x') = pickFreshName ctx x in
-      "(lambda " ++ x' ++ ". " ++ printTm ctx' t1 ++ ")"
-  TmApp t1 t2 ->
-    "(" ++ printTm ctx t1 ++ " " ++ printTm ctx t2 ++ ")"
+newtype Context = Context [(Name, Binding)]
+
+-- Context management
+
+emptyContext :: Context
+emptyContext = Context []
+
+ctxLength :: Context -> Int
+ctxLength (Context ctx) = length ctx
+
+addBinding :: Context -> Name -> Binding -> Context
+addBinding (Context ctx) name bind = Context $ (name, bind) : ctx
+
+addName :: Context -> Name -> Context
+addName ctx name = addBinding ctx name NameBind
+
+isNameBound :: Context -> Name -> Bool
+isNameBound (Context ctx) name =
+  any (\element -> fst element == name) ctx
+
+-- TODO: replace string with Name
+
+pickFreshName :: Context -> Name -> (Context, Name)
+pickFreshName ctx name =
+  if isNameBound ctx name then
+    pickFreshName ctx (name ++ "'")
+  else
+    (addName ctx name, name)
+
+index2name :: Context -> Int -> Maybe Name
+index2name (Context ctx) index =
+  fst <$> maybeElement
   where
-    pickFreshName ctx x = (ctx, x) -- TODO: implement me.
-    ctxLength (Context ctx) = length ctx
-    index2name :: Context -> Int -> String
-    index2name (Context ctx) x =
-      fromMaybe "[index not in context]" (fst <$> maybeElement)
-      where
-        maybeElement = find (\contextElement -> snd contextElement == x) ctx
+    maybeElement :: Maybe (String, Binding)
+    maybeElement
+      | index >= length ctx = Nothing
+      | otherwise = Just $ ctx !! index
+
+name2index :: Context -> Name -> Maybe Int
+name2index (Context ctx) name =
+  testIndex ctx name 0
+  where
+    testIndex :: [(Name, Binding)] -> Name -> Int -> Maybe Int
+    testIndex ctx name currentIndex
+      | null ctx = Nothing
+      | name == fst (head ctx) = Just currentIndex
+      | otherwise = testIndex (tail ctx) name (currentIndex + 1)
+
+
+-- printTm :: Context -> Term -> String
+-- printTm ctx t = case t of
+--   TmVar x n | ctxLength ctx == n -> show (index2name ctx x)
+--             | otherwise -> "[bad index]"
+--   TmAbs t1 x ->
+--     let (ctx', x') = pickFreshName ctx x in
+--       "(lambda " ++ x' ++ ". " ++ printTm ctx' t1 ++ ")"
+--   TmApp t1 t2 ->
+--     "(" ++ printTm ctx t1 ++ " " ++ printTm ctx t2 ++ ")"
+--   where
 
 
 termShift :: Int -> Term -> Term
